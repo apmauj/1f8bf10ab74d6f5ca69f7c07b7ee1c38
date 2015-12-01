575d79169624bc931e0bd0c99d52c17f850f6624
apmauj <jrozada@gmail.com>
2015-12-01 17:35:06 -0300

    Cambios al login mobile

--- frontend/views/mobile/login.php
+++ frontend/views/mobile/login.php
@@ -2,52 +2,55 @@
 $asset = frontend\assets\AppAsset::register($this);
 $baseUrl = $asset->baseUrl;
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = Yii::t('mobile', 'Muli Relevadores');
 ?>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery Mobile: Theme Download</title>
+    <title><?= Yii::t('mobile', 'Muli Relevadores Mobile');?></title>
     <link rel="stylesheet" href="/frontend/views/mobile/themes/mulisrelevadores.min.css" />
     <link rel="stylesheet" href="/frontend/views/mobile/themes/jquery.mobile.icons.min.css" />
     <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 </head>
-<body>
+<body >
 
+    <div class="ui-content ui-page-theme-a" data-form="ui-page-theme-a" data-theme="a" role="main" data-role="page" id="rutas"
+         style="background-image: url(<?=$baseUrl?>/images/mulirelevadores/background.png);background-size: 100% 100%;background-repeat: no-repeat;background-position: center">
 
+        <img src="<?=$baseUrl?>/images/mulirelevadores/banner.jpg" style="height: 100px; width: 90%;" alt="logo"><br>
 
-<div class="ui-content ui-page-theme-a" data-form="ui-page-theme-a" data-theme="a" role="main" data-role="page" id="rutas">
+        <div data-role="fieldcontain" style="width: 90%;">
+            <label for="name" style="color: white;"><?= Yii::t('mobile', 'Username');?></label>
+            <input type="text" name="name" id="name" value=""   />
+        </div>
 
-    <div data-role="fieldcontain">
-        <label for="name"><?= Yii::t('mobile', 'Username');?></label>
-        <input type="text" name="name" id="name" value="" />
+        <div data-role="fieldcontain" style="width: 90%;">
+            <label for="password" style="color: white;"><?= Yii::t('mobile', 'Password');?></label>
+            <input type="password" name="password" id="password"/>
+        </div>
+
+        <div data-role="controlgroup" data-type="vertical" style="width: 90%;">
+            <input type='button' id="login" value='<?= Yii::t('mobile', 'Log In');?>'>
+        </div>
+
+        <div id="mensaje">
+
+        </div>
+
+        <div id="producto">
+
+        </div>
+
+        <footer id="footer" class="midnight-blue">
+            <div class="col-sm-6" style="text-align: right; width: 90%;">
+                <b><?= Yii::t('app', 'All Rights Reserved. &copy; 2015.')?></b>
+            </div>
+        </footer><!--/#footer-->
+
     </div>
-
-    <div data-role="fieldcontain">
-        <label for="password"><?= Yii::t('mobile', 'Password');?></label>
-        <input type="password" name="password" id="password" value=""/>
-    </div>
-
-    <div data-role="controlgroup" data-type="vertical">
-        <input type='button' id="login" value='<?= Yii::t('mobile', 'Log In');?>'>
-    </div>
-
-    <div id="mensaje">
-
-    </div>
-
-
-    <div id="producto">
-
-    </div>
-
-
-
-
-
 
     <script>
         if (typeof window.localStorage != "undefined") {
@@ -73,7 +76,6 @@ $this->title = 'My Yii Application';
                             if (typeof window.localStorage != "undefined") {
                                 localStorage.setItem("muli_token", response.access_token);
                                 window.location = '/frontend/web/mobile/index';
-
                             }else{
                                 alert("Error... su navegador deberia soportar almacenamiento local. Pero no. Tremenda Basura");
                             }
@@ -83,51 +85,13 @@ $this->title = 'My Yii Application';
                     },
                     error : function(xhr, ajaxOptions, thrownError){
                         var err = eval("(" + xhr.responseText + ")");
-                        //alert(err.message);
-
                         $('#mensaje').html('<p class="error">'+err.message+'</p>') ;
                         $('#mensaje').show()
                     }
                 });
             });
-
-           /* $.ajax({
-                url: '/api/web/v2/producto',
-                method : 'GET',
-                dataType : 'json',
-                success : function(productos){
-                    console.log('productos', productos)
-                    var divPRod = $('#producto');
-                    var html = '';
-
-                    $('#mensaje').html('ta todo bien')
-
-                    setTimeout(function(){
-                        $('#mensaje').show();
-                    }, 3000);
-
-                    $.each( productos, function(key, producto ){
-                        console.log(producto.nombre);
-
-                        html += '<h1>' + producto.nombre + '</h1>';
-                        html += '<p>' + producto.precio + '</p>';
-                        html += '<button>comprar</button>';
-
-                        //divPRod.prepend('<h1>' + producto.nombre + '</h1> ');
-
-                    });
-
-                    divPRod.html(html);
-                },
-                error : function(){
-                    alert('error')
-                }
-            });*/
         });
     </script>
-
-
-</div>
 
 </body>
 </html>
